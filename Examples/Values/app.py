26edc1ebcf3146360aa8c5f58d5429f75038dda4
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-01-26 23:49:27 +0500

    done

--- Examples/Values/app.py
+++ Examples/Values/app.py
@@ -1,6 +1,6 @@
 print("Py" + "thon")
-print("1+1 =", 1+1)
-print("7.0/3.0 =", 7.0/3.0)
+print("1+1 = ", 2)
+print("7.0/3.0 = ", 7.0/3.0)
 print(True and False)
 print(True or False)
 print(not True)
