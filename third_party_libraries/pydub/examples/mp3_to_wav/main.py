2b885a57a94e503f4be58c9391300c80f78144da
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-02-21 21:27:21 +0500

    done

---
+++ third_party_libraries/pydub/examples/mp3_to_wav/main.py
@@ -0,0 +1,11 @@
+from pydub import AudioSegment
+
+# Replace with your file paths
+mp3_file = "never-gonna-give-you-up.mp3"
+wav_file = "never-gonna-give-you-up.wav"
+
+# Read MP3 and export as WAV
+audio = AudioSegment.from_mp3(mp3_file)
+audio.export(wav_file, format="wav")
+
+print("Converted MP3 to WAV successfully!")
