bfdb71985d12a63154de939c55543ca7dcad8c6a
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-02-22 18:47:24 +0500

    done

---
+++ third_party_libraries/pydub/examples/mp3_to_mp4a/main.py
@@ -0,0 +1,6 @@
+# Unfortunately, pydub currently doesn't support native 
+# conversion of MP3 to MP4A due to limitations with the 
+# underlying library it uses (libav) on M1 Macs. However, 
+# there are still ways to achieve this conversion using 
+# pydub in conjunction with other tools: https://g.co/gemini/share/a95ce5d1d79a
+
