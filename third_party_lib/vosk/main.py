8ca50fde657c7fc06a71cc70eb7e71a8af0fd4e9
shakhzodtojiev <tojiyevshakhzod@gmail.com>
2024-03-20 00:48:05 +0500

    done

---
+++ third_party_lib/vosk/main.py
@@ -0,0 +1,28 @@
+import wave
+import json
+from vosk import Model, KaldiRecognizer
+
+def stt(wav_file):
+    # Open the wave file
+    with wave.open(wav_file, "rb") as wf:
+        # Load the Vosk model 
+        model = Model(r"./vosk-model-small-uz-0.22")
+        # Create a KaldiRecognizer
+        rec = KaldiRecognizer(model, wf.getframerate())
+
+        while True:
+            data = wf.readframes(wf.getnframes())  # Process audio in chunks
+
+            if len(data) == 0:
+                break
+
+            if rec.AcceptWaveform(data):
+                result = json.loads(rec.Result())
+                return result["text"]
+            else:
+                # Handle partial results if needed
+                pass
+
+wav_file = "/Users/shakhzodtojiev/Downloads/Coding/Python-Tutorials/media/temp.wav"
+result_text = stt(wav_file)
+print("Recognized text:", result_text)
