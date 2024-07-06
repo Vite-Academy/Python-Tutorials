cf8230bab7f40e29b517135a7755c64484d0da42
shakhzodtojiev <tojiyevshakhzod@gmail.com>
2024-07-06 16:53:26 +0500

    done

---
+++ Examples/fizz_buzz/strong.py
@@ -0,0 +1,48 @@
+# <--- Interview question --->
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["1","2","Fizz"]
+# Example 2:
+
+# Input: n = 5
+# Output: ["1","2","Fizz","4","Buzz"]
+# Example 3:
+
+# Input: n = 15
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+# Constraints:
+
+# 1 <= n <= 10^4
+
+# <--- Solution --->
+
+from typing import List
+
+def fizzBuzz(n: int) -> List[str]:
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return result
+
+print(fizzBuzz(15))
+
+
+# Video tutorials [eng]:
+# 1. Timothy H Chang: https://www.youtube.com/watch?v=TMm1CPhM5i4
